4ae42de2bb4275371f316b3ffff8da66d5fb5bde
apiedy <abhishek.piedy@gmail.com>
2017-06-13 00:16:34 -0400

    Add Bootstrap Table UI

--- js/controllers/displayController.js
+++ js/controllers/displayController.js
@@ -1,7 +1,42 @@
 app.controller('DisplayController', ['$scope', 'Operations', function($scope,Operations) {
-	
-	Operations.getData().then(function(response){
-		$scope.details = response;
-	});
+
+		$("#displayTable").bootstrapTable({
+			url: "http://localhost:3030/students",
+            toolbar: '#toolbar',
+            striped: true,
+            search: true,
+            showColumns: true,
+            showToggle: true,
+            showRefresh: true,
+            pagination: true,
+            sortName: 'fullName',
+            sortOrder: 'asc',
+            pageSize: 10,
+            pageList: [5,10,25,50,100],
+			columns: [{
+                field: 'fullName',
+                title: 'Full Name',
+                sortName: 'fname',
+                sortable: true,
+                switchable: false,
+                formatter: function (value, row) {
+                    var name = [row.fname, row.lname].join(' ');
+                    return '<a href="#!/display/'+ row.id +'">'+name+'</a>';
+                }
+            }, {
+                field: 'age',
+                title: 'Age',
+                sortable:true,
+                visible: false
+            }, {
+            	field: 'degree',
+            	title: 'Degree',
+            	sortable: true
+            }, {
+            	field: 'college',
+            	title: 'College',
+            	sortable: true
+            }]
+		})
 	
 }]);
